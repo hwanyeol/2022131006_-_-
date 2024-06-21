5f4eac6a23c763301d7d0948a9d6330ca8f290fa
hwanyeol <162323167+hwanyeol@users.noreply.github.com>
2024-06-21 10:52:24 +0900

    Delete 수프밍 directory

--- 수프밍/202.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isHappy(self, n: int) -> bool:
-        seen = set()  
-        while n != 1:
-            if n in seen:  # 이미 본 숫자를 만나면, 순환에 빠진 것
-                return False
-            seen.add(n)
-            n = sum(int(digit) ** 2 for digit in str(n))  
-        return True 
